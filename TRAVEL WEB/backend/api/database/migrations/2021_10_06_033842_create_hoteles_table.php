a68f17038508fc946996a59fe11df1418014aff7
Crist-C <castro.cristiank@gmail.com>
2021-10-06 02:24:41 -0500

    Se inicia con el proceso de creación de la API. En donde se instaló composer, poseteriormente Laravel y por medio del comando Artisan se crearon las tablas en la base de datos. Para la DB se esta usando MySql con XAMPP.

---
+++ TRAVEL WEB/backend/api/database/migrations/2021_10_06_033842_create_hoteles_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHotelesTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('hoteles', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 50);
+            $table->string('ciudad', 50);
+            $table->tinyInteger('estrellas');
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('hoteles');
+    }
+}
